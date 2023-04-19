2c98f31a36e84f15c4b81614de80f6b3932f0a89
wheelerjl <wheelerjl.dev@gmail.com>
2023-04-18 23:54:44 -0500

    add migration and taskfile pattern

--- migrations/20230418230750_add_initial.sql
+++ migrations/20230418230750_add_initial.sql
@@ -1,18 +1,23 @@
+-- +goose Up
 CREATE SCHEMA godoit;
-SET SCHEMA 'godoit';
-
-CREATE TABLE subjects (
+CREATE TABLE godoit.subjects (
   subject_id UUID PRIMARY KEY,
   name VARCHAR NOT NULL,
   image_url TEXT
 );
 
-CREATE TABLE activities (
+CREATE TABLE godoit.activities (
   activity_id UUID PRIMARY KEY,
-  subject_id UUID NOT NULL REFERENCES subjects(subject_id),
+  subject_id UUID NOT NULL REFERENCES godoit.subjects(subject_id),
   assigned_user_id TEXT,
   name TEXT NOT NULL,
   location TEXT,
   description TEXT,
   start_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
+
+-- +goose Down
+DROP TABLE godoit.activities;
+DROP TABLE godoit.subjects;
+DROP SCHEMA godoit;
+
